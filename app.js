ca302986857cf2ed46826ff584fb6f68b13170ab
Раджабов Роман <38882414+RadzhabovRoman@users.noreply.github.com>
2019-11-30 23:04:52 +0500

    +ADD() in DB

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true}));
 
-const value = 'hi';
-
-app.get('/', (req, res) => res.render('index', { data: value }));
+app.get('/', (req, res) => res.render('index'));
 
 app.get('/create', (req, res) => res.render('create'));
 app.post('/create', (req, res) => {
-	console.log(req.body);
+	const {name} = req.body;
+
+	User.create({
+		name: name
+	}).then(user => console.log(user));		
+	
+	res.redirect('/');
 });
 
 module.exports = app;
