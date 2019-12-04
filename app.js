805a035d445baee9fed8c1a5ce33fb51e1b64bfb
Раджабов Роман <38882414+RadzhabovRoman@users.noreply.github.com>
2019-12-04 23:49:32 +0500

    elementary authorization

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const User = require('./models/user');
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true}), cookieParser());
 
-/*app.get('/', (req, res) => {
-	//User.find({name: 'Borovik'})
-	User.find({})
-    .then(users => {
-      res.render('index', { users: users });
-    })
-    .catch(err => {
-      res.status(200).json({ err: err });
-    });
+app.get('/', (req, res) => {
+	res.render('index');
 });
-*/
-app.get('/', (req, res) => res.render('index'));
 
-app.get('/user_reg', (req, res) => res.render('user_reg'));
+app.get('/user_reg', (req, res) => res.render('user_reg')); //если куки подойдет, то рендерим
 app.post('/user_reg', (req, res) => {
-	console.log(req.body);
 	const {number, password, name} = req.body;
-	//User.remove({
 	User.create({
 		number: number,
 		password: password,
@@ -33,8 +23,32 @@ app.post('/user_reg', (req, res) => {
 		balance: 0,
 		serv: '0'
 	}).then(user => console.log(user));		
-	
 	res.redirect('/');
+});
+
+
+app.get('/user_login', (req, res) => res.render('user_login')); 
+app.post('/user_login', (req, res) => {
+	const {number, password, name} = req.body;
+	User.find({
+		number: number,
+		password: password
+	}).then(user => {
+		if (user[0]) {
+			res.cookie('role', 'user'); // отправили куку
+			res.redirect('/user_cabinet');
+		}
+		else {
+			res.redirect('/');
+		}
+	})
+	.catch(() => console.log('auth_error'));
+});
+
+app.get('/user_cabinet', (req, res) =>  {
+	if (req.cookies.role === 'user') {
+		res.render('user_cabinet')
+	}
 });
 
 module.exports = app;
